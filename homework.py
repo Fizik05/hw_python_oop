4369e2424a62a21f3aac434c3eb9e19521ad0389
Богдан <bboybaga13@mail.ru>
2021-06-16 09:37:45 +0300

    My last commit

--- homework.py
+++ homework.py
@@ -5,66 +5,25 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.week = dt.date.today() - dt.timedelta(days=7)
-        self.today = dt.date.today()
 
     def add_record(self, someone):
         self.records.append(someone)
 
     def get_today_stats(self):
-        today_stats = 0
-        for i in self.records:
-            if i.date == self.today:
-                today_stats += i.amount
-        return today_stats
+        today = dt.date.today()
+        return sum(
+            record.amount
+            for record in self.records if record.date == today
+        )
 
     def get_week_stats(self):
-        week_stats = 0
-        for i in self.records:
-            if self.week <= i.date <= self.today:
-                week_stats += i.amount
-        return week_stats
-
-
-class CashCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-
-    RUB_RATE: float = 1
-    EURO_RATE = 87.39
-    USD_RATE = 71.78
-
-    def get_today_cash_remained(self, currency='rub'):
-        currency_dict = {'rub': (self.RUB_RATE, 'руб')}
-        minus = self.limit - self.get_today_stats()
-        if minus == 0:
-            return('Денег нет, держись')
-        currency_dict['eur'] = (self.EURO_RATE, 'Euro')
-        currency_dict['usd'] = (self.USD_RATE, 'USD')
-        try:
-            value = minus / currency_dict[currency][0]
-            currenc = currency_dict[currency][1]
-        except Exception:
-            return('Это не существующая валюта')
-        if value > 0:
-            return(f'На сегодня осталось {value:.2f} '
-                   f'{currenc}')
-        else:
-            return('Денег нет, держись: твой долг - '
-                   f'{abs(value):.2f} {currenc}')
-
-
-class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-
-    def get_calories_remained(self):
-        today_ate = self.limit - self.get_today_stats()
-        if today_ate > 0:
-            return ('Сегодня можно съесть что-нибудь ещё, но с общей '
-                    f'калорийностью не более {today_ate} кКал')
-        else:
-            return 'Хватит есть!'
+        week = dt.date.today() - dt.timedelta(days=7)
+        today = dt.date.today()
+        return sum(
+            record.amount
+            for record in self.records
+            if week < record.date <= today
+        )
 
 
 class Record(Calculator):
@@ -77,9 +36,43 @@ class Record(Calculator):
         self.comment = comment
 
 
+class CashCalculator(Calculator):
+    RUB_RATE = 1.0
+    EURO_RATE = 87.39
+    USD_RATE = 71.78
+
+    def get_today_cash_remained(self, currency='rub'):
+        currency_dict = {'rub': (self.RUB_RATE, 'руб'),
+                         'eur': (self.EURO_RATE, 'Euro'),
+                         'usd': (self.USD_RATE, 'USD')}
+        minus = self.limit - self.get_today_stats()
+        if minus == 0:
+            return 'Денег нет, держись'
+        if currency not in currency_dict:
+            raise ValueError('Плохая валюта')
+        else:
+            value = minus / currency_dict[currency][0]
+            currency_1 = currency_dict[currency][1]
+        if value > 0:
+            return(f'На сегодня осталось {value:.2f} '
+                   f'{currency_1}')
+        modul = abs(value)
+        return('Денег нет, держись: твой долг - '
+               f'{modul:.2f} {currency_1}')
+
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        today_ate = self.limit - self.get_today_stats()
+        if today_ate > 0:
+            return ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                    f'калорийностью не более {today_ate} кКал')
+        return 'Хватит есть!'
+
+
 cash_calculator = CashCalculator(1000)
 calories_calculator = CaloriesCalculator(3000)
-cash_calculator.add_record(Record(amount=85674, comment='Серёге за обед'))
+cash_calculator.add_record(Record(amount=1, comment='Серёге за обед'))
 cash_calculator.add_record(Record(amount=145, comment='кофе'))
 calories_calculator.add_record(Record(amount=4145, comment='кофе'))
 
